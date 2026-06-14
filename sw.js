81f12f66ed392e3847947ae0b30b6dae8834bf73
legolasarewn <legolasarewn@gmail.com>
2026-06-14 18:24:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,3 @@
-const CACHE = 'worldcup2026-v2';
-const ASSETS = ['./index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-});
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
